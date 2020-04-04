b7d153db0fb1880788fb159e2e46d89c656b91a8
若云 <zyhdvlp@gmail.com>
2020-04-04 10:36:01 +0800

    压缩c++代码，从250k+ 到 50k

--- app/src/main/java/vip/ruoyun/test/MainActivity.java
+++ app/src/main/java/vip/ruoyun/test/MainActivity.java
@@ -2,6 +2,7 @@ package vip.ruoyun.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import vip.ruoyun.update.AppUpdate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        AppUpdate.bspatch("", "", "");
     }
 }
